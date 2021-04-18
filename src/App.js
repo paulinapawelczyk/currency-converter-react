660d9be9b23614226c61ad58449aa46de5ff1b16
PaulinaPawelczyk <paulinapatrycja.fior@gmail.com>
2021-04-18 17:11:17 +0200

    Adjust CSS files to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import './reset.css';
 import './App.css';
+import logo from './assets/logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <p>aaa</p>
+    <div className="container">
+      <div className="app">
+        <div className="header">
+          <h1>Currency converter</h1>
+        </div>
+        <div className="logo">
+          <img src={logo} alt="Converter logo"></img>
+        </div>
+        <div className="forms">
+          <p className="error-message"></p>
+          <form className="amount-input">
+            <input
+              type="number"
+              placeholder="Amount to convert"
+              id="amount-value"
+            ></input>
+            <select name="amount-from" id="amount-code">
+              <option value="EUR">EUR</option>
+              <option value="USD">USD</option>
+              <option value="CHF">CHF</option>
+            </select>
+            <button id="btnCount" type="submit">
+              Count
+            </button>
+          </form>
+        </div>
+        <div className="output">
+          <div className="rate">------</div>
+          <div className="exchangeResult">0</div>
+          <p>PLN</p>
+        </div>
+      </div>
     </div>
   );
 }
