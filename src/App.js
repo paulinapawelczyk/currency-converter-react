91da0b61d11277cf6b5e2512366346fb395932f0
PaulinaPawelczyk <paulinapatrycja.fior@gmail.com>
2021-04-18 21:34:38 +0200

    Add fetch currencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React, { Component } from 'react';
 import './reset.css';
 import './App.css';
 import logo from './assets/logo.jpg';
 import ExchangeForm from './Components/ExchangeForm';
-import ExchangeResultPanel from './Components/ExchangeResultPanel';
 
-function App() {
-  return (
-    <div className="container">
-      <div className="app">
-        <h1 className="header">Currency converter</h1>
-        <div className="logo">
-          <img className="logo" src={logo} alt="Converter logo"></img>
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="app">
+          <h1 className="header">Currency converter</h1>
+          <div className="logo">
+            <img className="logo" src={logo} alt="Converter logo"></img>
+          </div>
+          <ExchangeForm />
         </div>
-        <ExchangeForm />
-        <ExchangeResultPanel />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Components/ExchangeForm.js
+++ src/Components/ExchangeForm.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
-
+import ExchangeResultPanel from './ExchangeResultPanel';
 class ExchangeForm extends Component {
+  state = {
+    amount: 0,
+    fromCurrency: '',
+    toCurrency: 'PLN',
+    currencies: [],
+    result: 0,
+  };
+
+  fetchUrl = `https://api.nbp.pl/api/exchangerates/tables/a/last/`;
+
+  componentDidMount() {
+    const currencyTableApi = [];
+    let currencyMap = [];
+
+    fetch(this.fetchUrl)
+      .then((result) => result.json())
+      .then((response) => {
+        for (const key in response[0].rates) {
+          currencyTableApi.push(response[0].rates[key]);
+          currencyMap = currencyTableApi.map((curr, index) => ({
+            ...curr,
+            index,
+          }));
+        }
+        this.setState({ currencies: currencyMap });
+        console.log(currencyMap);
+      })
+      .catch((err) => console.log(err));
+  }
+
   render() {
     return (
-      <div className="forms">
-        <p className="error-message"></p>
-        <form className="amount-input">
-          <input
-            type="number"
-            placeholder="Amount to convert"
-            id="amount-value"
-          ></input>
-          <select name="amount-from" id="amount-code">
-            <option value="EUR">EUR</option>
-            <option value="USD">USD</option>
-            <option value="CHF">CHF</option>
-          </select>
-          <button id="btnCount" type="submit">
-            Count
-          </button>
-        </form>
-      </div>
+      <>
+        <div className="forms">
+          <p className="error-message"></p>
+          <form className="amount-input">
+            <input
+              type="number"
+              placeholder="Amount to convert"
+              id="amount-value"
+              value={this.state.amount}
+              onChange={(e) => this.setState({ amount: e.target.value })}
+            ></input>
+            <select
+              name="amount-from"
+              id="amount-code"
+              value={this.state.fromCurrency}
+              onChange={(e) => this.handleSelectCurrency(e)}
+            >
+              {this.state.currencies.map((curr) => (
+                <option key={curr.index}>{curr.code}</option>
+              ))}
+            </select>
+            <button id="btnCount" type="submit">
+              Count
+            </button>
+          </form>
+        </div>
+        <ExchangeResultPanel />
+      </>
     );
   }
 }
